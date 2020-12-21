b99051ca75ac7d702175f1c3f8ff9d6a889e2fc6
Valeriy Shagur <vshagur@gmail.com>
2020-12-21 11:13:09 +0700

    python package structure created

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README", "r") as fh:
+    long_description = fh.read()
+
+setup(name='exgrex',
+      version='0.1.1',
+      description='Exgrex implementation for the Python programming language.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='vshagur',
+      author_email='vshagur@gmail.com',
+      packages=find_packages(exclude=['tests', '*.test.*', '*.test', ]),
+      scripts=[],
+      keywords=['mooc', 'grader', 'python', 'python3', 'education'],
+      url='https://github.com/vshagur/exgrex-py',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: POSIX :: Linux", ],
+      )
